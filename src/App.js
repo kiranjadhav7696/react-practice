a8f7106ab4c8e5b6ab84efc83015217c06e1d2ea
kiranjadhav7696 <kiran.jadhav7696@gmail.com>
2022-06-23 18:38:37 +0530

    first project on git

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import  Props  from "./components/Props"
+
+function App() {
+  return (
+    <div className="App">
+      <>
+        <Props name = "Kiran"/>
+      </>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Props.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Props = ({name}) => {
+return(
+    <>
+        <h1>Hello ,{name}</h1>
+    </>
+)
+}
+
+export default Props
